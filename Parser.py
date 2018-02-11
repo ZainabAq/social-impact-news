826eb7e04b041c756487172062fe10d620f6468e
Zainab Aqdas <zainab.aqdasrizvi@gmail.com>
2018-02-11 14:30:07 -0500

    Updated parser that works.

--- Parser.py
+++ Parser.py
@@ -5,13 +5,13 @@ import csv
 import zlib
 from lxml import html
 
-def parse(soup, new_rows_list):
+def parse(soup, url, file):
     tagList = soup.findAll("div", {"class": "entry__text js-entry-text bn-entry-text yr-entry-text"})
 
     for tag in tagList:
         if tagCheck(tag) == True:
             text = cleanText(tag);
-        new_rows_list.append(str(text)[1:-1]+"\n")
+    file.write(url + "," + str(text)[1:-1] + "\n")
 
 def tagCheck(tag):
     contents = tag.findAll("div", {"class":"content-list-component bn-content-list-text yr-content-list-text text"})
@@ -21,30 +21,30 @@ def tagCheck(tag):
         return True
 
 def cleanText(tag):
-        tagcodeList = tag.findAll("div", {"class":"content-list-component bn-content-list-text yr-content-list-text text"})
-        codeList = []
-        for code in tagcodeList:
+        codeList = ""
+        for code in tag:
             code = code.get_text(" ",strip=True)
-            codeList.append(code)
-        return codeList
+            codeList+=" "+code
+        return codeList;
 
 def main():
-    new_rows_list = []
     csv_file1 = open(sys.argv[1], 'r')
+    csv_file2 = open('text-data.csv', 'w', encoding="utf8")
+    writer = csv.writer(csv_file2, delimiter=',')
     reader = csv.reader(csv_file1)
     for row in reader:
-        address = str(row[0])
-        print(address)
-        html = requests.get(address, stream=True)
-        html = html.text
-        soup = BeautifulSoup(html, 'html5lib')
-        parse(soup, new_rows_list)
+        url = str(row[0])
+        fullText = ""
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+        html = requests.get(url, headers=headers)
+        html = html.content.decode('utf-8', 'ignore')
+        soup = BeautifulSoup(html, 'html.parser')
+        for chunk in soup.findAll("div", {"class":"content-list-component bn-content-list-text yr-content-list-text text"}):
+            chunk = cleanText(chunk);
+            fullText+=(chunk);
+        writer.writerow([url]+[fullText])
     csv_file1.close()
-
-    csv_file2 = open(sys.argv[1], 'w')
-    writer = csv.writer(csv_file2)
-    writer.writerows(new_rows_list)
-    file2.close()
+    csv_file2.close()
 
     print('Parsing complete!')
 
